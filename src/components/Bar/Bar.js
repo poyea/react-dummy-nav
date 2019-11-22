9ddfa68f2ad3599b40d35a3cadd43676925389d1
John Law <johnlaw.po@gmail.com>
2019-11-23 03:13:26 +0800

    Add fisrt Bar component

---
+++ src/components/Bar/Bar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default bar = props =>(
+    <header>
+        <nav>
+            <div></div>
+            <div><a href="/">Home</a></div>
+            <div>
+                <ul>
+                    <li><a href="/">Foo</a></li>
+                    <li><a href="/">Bar</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+);
