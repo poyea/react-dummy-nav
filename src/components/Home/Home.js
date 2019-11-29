8ca0287723567d6570b10c4a883e3fb178b95701
John Law <johnlaw.po@gmail.com>
2019-11-30 00:28:54 +0800

    Fix Invariant Violation

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,11 +3,11 @@ import {
     useLocation
 } from "react-router-dom";
 
-const Home = () => {
+function Home() {
     let location = useLocation();
     return (
-            <p>This is the home page from {location.pathname}!</p>
+        <p>This is the home page from {location.pathname}!</p>
     );
-};
+}
 
 export default Home;
