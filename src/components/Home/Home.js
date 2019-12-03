e1160de3b784ae9b9e8065d3b7868a8340840a11
John Law <johnlaw.po@gmail.com>
2019-12-03 23:59:00 +0800

    Attempt to fix routing

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-class Home extends React.Component {
-    render() {  
-      return (
-        <p>This is the home page!</p>
-        );
-    }
-}
+const Home = () => {
+  return (
+    <div>
+      <p>This is the home page!</p>
+    </div>
+  );
+};
 
 export default Home;
